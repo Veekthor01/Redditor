196510760c8601dfb4bc304a922f98c833299f74
Veekthor01 <veekthor01@gmail.com>
2024-01-05 23:50:49 +0300

    Refactor popular posts and posts pages

--- app/@PopularPosts/page.js
+++ app/@PopularPosts/page.js
@@ -1,4 +1,60 @@
+'use client'
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
+import './popularPosts.css';
+
+const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+async function getPopularPosts() {
+    const posts = `${backendUrl}/api/redditPopularPost`;
+    try { 
+        const response = await fetch(posts);
+        const data = await response.json();
+        return data.posts;
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
+
+export default function PopularPosts() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        async function fetchPosts() {
+            const posts = await getPopularPosts();
+            setPosts(posts);
+        }
+
+        fetchPosts();
+    }, []);
+
+    return (
+        <div className="p-6 pl-1 max-w-sm items-center space-x-4 overflow-auto h-screen scrollbar-hide md:block hidden">
+            <h1 className="text-lg pb-2 text-gray-100 text-center font-bold">Reddit Popular Posts</h1>
+            <h2 className="text-sm text-gray-100 text-center">These are the top 25 posts on reddit</h2>
+            <ul className="list-none list-inside">
+                {posts.map((post, index) => (
+                    <li key={`${post.subreddit}-${post.title}-${post.timestamp}`} className="border-t border-gray-200">
+                        <Link href={post.url} target="_blank" rel="noopener noreferrer">
+                        <div className="p-4">
+                            <h2 className="lg:text-base text-sm text-red-400 font-bold pb-1">
+                            <span className="mr-3">{index + 1}.</span>
+                                <span>{post.subreddit}</span>
+                            </h2>
+                            <p className="text-sm md:text text-gray-300 pb-1">{post.title}</p>
+                            <p className="text-sm text-gray-400 pb-1">{post.author}</p>
+                            <p className="text-sm text-gray-500">{post.timestamp}</p>
+                        </div>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+/*import Link from 'next/link';
 import delay from '@/lib/delay';
 import './popularPosts.css';
 
@@ -42,4 +98,4 @@ export default async function PopularPosts() {
             </ul>
         </div>
     );
-} 
+} */
--- app/@Posts/page.js
+++ app/@Posts/page.js
@@ -2,6 +2,85 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { FiArrowLeft, FiArrowRight } from 'react-icons/fi';
+//import { getPosts } from './fetchPost';
+import delay from '@/lib/delay';
+import LoadingPosts from '@/lib/loading';
+
+export default function Posts() {
+    const [loading, setLoading] = useState(true);
+    const [posts, setPosts] = useState([]);
+    // The index of the first posts to display
+    const [currentIndex, setCurrentIndex] = useState(0);
+  
+    useEffect(() => {
+      async function fetchPosts() {
+        const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+        const postsUrl = `${backendUrl}/api/redditPost`;
+    
+        try {
+          await delay(2000);
+          const response = await fetch(postsUrl);
+          const data = await response.json();
+          setPosts(data.posts);
+          setLoading(false); // Set loading to false after fetching the posts
+        } catch (err) {
+          console.error(err);
+          setLoading(false); // Also set loading to false if an error occurs
+        }
+      }
+    
+      fetchPosts();
+    }, []);
+
+      if (loading) {
+        return <LoadingPosts />;
+      }
+  
+      // Display the next 9 posts
+    const next = () => {
+      setCurrentIndex((currentIndex + 9) % posts.length);
+    };
+  
+    // Display the previous 9 posts
+    const prev = () => {
+      setCurrentIndex((currentIndex - 9 + posts.length) % posts.length);
+    };
+  
+    return (
+        <div className="p-6">
+          <h1 className="text-xl pb-4 text-gray-100 text-center font-bold">Top stories</h1>
+          <div className="flex items-center">
+          {currentIndex > 0 && 
+            <button onClick={prev} className='text-white bg-red-500 rounded-full p-2 relative left-5'>
+              <FiArrowLeft />
+            </button>
+          }
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
+              {posts.slice(currentIndex, currentIndex + 9).map((post) => (
+                <div key={`${post._id}`} className="border rounded-xl p-2 md:p-4 h-64 overflow-auto">
+                    <Link href={post.url} target="_blank" rel="noopener noreferrer">
+                    <img src={post.img} alt={post.title} className="object-contain h-32 w-full" />
+                  <h2 className="text-base text-red-400 font-bold pb-1">{post.subreddit}</h2>
+                  <p className="text-sm text-gray-300 pb-1">{post.title}</p>
+                  <p className="text-sm text-gray-400 pb-1">{post.author}</p>
+                  <p className="text-sm text-gray-500">{post.timestamp}</p>
+                  </Link>
+                </div>
+              ))}
+            </div>
+            {currentIndex < posts.length - 9 && 
+              <button onClick={next} className='text-white bg-red-500 rounded-full p-2 relative right-5'>
+                <FiArrowRight />
+              </button>
+            }
+          </div>
+        </div>
+      );
+  }
+
+/*import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { FiArrowLeft, FiArrowRight } from 'react-icons/fi';
 import { getPosts } from './fetchPost';
 import delay from '@/lib/delay';
 import LoadingPosts from '@/lib/loading';
@@ -66,4 +145,4 @@ export default function Posts() {
           </div>
         </div>
       );
-  }
+  } */
